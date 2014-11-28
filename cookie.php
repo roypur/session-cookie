54e29c4e9e3a610057020729dfda2c3d6ee8ff76
Roy Olav Purser <admin@royolav.net>
2014-11-28 14:02:26 +0100

    Update cookie.php

--- cookie.php
+++ cookie.php
@@ -3,12 +3,14 @@
 $pass = "61e7680d2ac47e5b9e3c82118fae6e3cfcddff285ac75bb82872bb01f24ac657";
 
 function valCookie(){
-    $cookie = json_decode(hex2bin($_COOKIE['session']), true);
-    global $pass;
-    $hash = hash('sha256', $_SERVER['REMOTE_ADDR'] . $cookie['uid'] . $cookie['expiry'] . $pass);
-    $uid = $cookie['uid'];
-    if ((strcmp($hash, $cookie['hash']) == 0) && ($cookie['expiry'] > time())){
-        return $uid; //return user id.
+    if (isset($_COOKIE['session'])){
+        $cookie = json_decode(hex2bin($_COOKIE['session']), true);
+        global $pass;
+        $hash = hash('sha256', $_SERVER['REMOTE_ADDR'] . $cookie['uid'] . $cookie['expiry'] . $pass);
+        $uid = $cookie['uid'];
+        if ((strcmp((string)$hash, (string)$cookie['hash']) == 0) && ($cookie['expiry'] > time())){
+            return $uid; //return user id.
+            }
         }
     }
 function hashCookie($uid, $expiry){
