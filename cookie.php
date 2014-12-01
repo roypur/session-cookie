ea78a62da4dc6908f15f3cee6f78c650f5d9f62e
Roy Olav Purser <admin@royolav.net>
2014-12-01 18:07:38 +0100

    Update cookie.php

--- cookie.php
+++ cookie.php
@@ -1,12 +1,12 @@
 <?php
 
-$pass = "61e7680d2ac47e5b9e3c82118fae6e3cfcddff285ac75bb82872bb01f24ac657";
+$key = "61e7680d2ac47e5b9e3c82118fae6e3cfcddff285ac75bb82872bb01f24ac657";
 
 function valCookie(){
     if (isset($_COOKIE['session'])){
         $cookie = json_decode(hex2bin($_COOKIE['session']), true);
-        global $pass;
-        $hash = hash_hmac('sha256', $_SERVER['REMOTE_ADDR'] . $cookie['uid'] . $cookie['expiry'], $pass);
+        global $key;
+        $hash = hash_hmac('sha256', $_SERVER['REMOTE_ADDR'] . $cookie['uid'] . $cookie['expiry'], $key);
         $uid = $cookie['uid'];
         if ((hash_equals($hash, $cookie['hash'])) && ($cookie['expiry'] > time())){
             return $uid; //return user id.
@@ -14,10 +14,10 @@ function valCookie(){
         }
     }
 function hashCookie($uid, $timeout = 3600){
-    global $pass;
+    global $key;
     $cookie['uid'] = $uid;
     $cookie['expiry'] = time()+$timeout;
-    $cookie['hash'] = hash_hmac('sha256', $_SERVER['REMOTE_ADDR'] . $cookie['uid'] . $cookie['expiry'], $pass);
+    $cookie['hash'] = hash_hmac('sha256', $_SERVER['REMOTE_ADDR'] . $cookie['uid'] . $cookie['expiry'], $key);
     $hexCookie = bin2hex(json_encode($cookie));
     setcookie("session", $hexCookie, $cookie['expiry']);
     if(strlen($uid)){
