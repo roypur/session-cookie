227422c098ab89d1c7956fda85cba70a59ef3233
Roy Olav Purser <admin@royolav.net>
2014-12-20 13:07:33 +0100

    update

--- time.php
+++ time.php
@@ -1,9 +1,4 @@
 <?php
-echo date("d M y G:i", time()+200);
-$db = new mysqli('127.0.0.1', 'root', '', 'new');
-$result = $db->query("select * from users");
-while($row = $result->fetch_assoc()){
-echo $row['name'] . "<br>";
-}
+
 
 ?>
